5f8a6b4835185a7783ef746f0d76ef42f0e7660d
simon <simon@DESKTOP-96F0NT5>
2023-07-07 09:31:15 +0700

    Add project files.

---
+++ LeoKayProject/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+
+namespace LeoKayProject.Controllers
+{
+    [Route("api/logincontroller")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        public IHttpActionResult GetData()
+        {
+            // Xử lý logic để lấy dữ liệu từ nguồn nào đó
+            var data = GetDataFromSource();
+
+            // Trả về dữ liệu dưới dạng JSON
+            return Json(data);
+        }
+
+        private object GetDataFromSource()
+        {
+            // Xử lý logic để lấy dữ liệu từ nguồn nào đó
+            // Ví dụ: truy vấn cơ sở dữ liệu, gọi API bên ngoài, vv.
+            // Trả về dữ liệu dưới dạng object
+            return new { Name = "John Doe", Age = 30 };
+        }
+
+    }
+}
---
+++ LeoKayProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeoKayProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
